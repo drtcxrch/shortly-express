5190e27f6e7144296a97461bf2a039fea1a53f6a
Anthony Sim <anthonyysim@gmail.com>
2021-01-22 10:05:30 -0800

    passed all tests for create sessions expect the last 2 tests

--- server/middleware/auth.js
+++ server/middleware/auth.js
@@ -2,35 +2,43 @@ const models = require('../models');
 const Promise = require('bluebird');
 
 module.exports.createSession = (req, res, next) => {
-  // checks to see if there is a hash
-  let hash = req.cookies.shortlyid;
-
-  // if there is no hash one is created
-  if (!hash) {
+  // if a hash does NOT exist...
+  if (!req.cookies.shortlyid) {
+    // create a hash and store it in the sessions db.
     models.Sessions.create()
-      // then we use the insertId to retrieve the hash that was created from the sessions table
-      // the sessions table has 2 columns id and hash, by using insertId we should be able to get the hash back
-      .then(results => models.Sessions.get({ id: results.insertId }))
-      // need to find a way set a cookie in the response header?!?!
-      // https://www.geeksforgeeks.org/express-js-res-cookie-function/
-      .then(data => res.cookie('shortlyid', data.hash));
-    next();
+      // then retrieve the data from the sessions db.
+      .then(data => models.Sessions.get({ id: data.insertId }))
+      // then store the hash in a sessions property under req
+      // and set the cookie to the header using res.cookie.
+      .then(databaseData => {
+        // console.log(databaseData);
+        req.session = databaseData;
+        res.cookie('shortlyid', req.session.hash);
+        next();
+      });
+
+    // if a hash DOES exist...
   } else {
-    models.Sessions.get({ hash })
-      .then(hasSession => {
-        if (hasSession) {
-          res.cookie('shortlyid', hasSession.hash);
+    // retrieve the data from the sessions db.
+    models.Sessions.get({ id: req.cookies.shortlyid })
+      .then(databaseData => {
+        // if the data exists, then everything is ok!
+        if (databaseData) {
           next();
+          // if the data does NOT exist...
         } else {
+          // repeat the steps taken when a hash did NOT exist above.
           models.Sessions.create()
-            .then(results => models.Sessions.get({ id: results.insertId }))
-            .then(data => res.cookie('shortlyid', data.hash));
-          next();
+            .then(data => models.Sessions.get({ id: data.insertId }))
+            .then(databaseData => {
+              req.session = databaseData;
+              res.cookie('shortlyid', req.session.hash);
+              next();
+            });
         }
       });
   }
 };
-
 
 
 // accesses the parsed cookies on the request,
@@ -39,10 +47,10 @@ module.exports.createSession = (req, res, next) => {
 // would you want to keep in this session object?)
 
 // CHECK! An incoming request with no cookies should generate a session with a unique hash and store it the sessions database.
-// CHECK! I think?!?!?! The middleware function should use this unique hash to set a cookie in the response headers. (Ask yourself: How do I set cookies using Express?).
+// CHECK! The middleware function should use this unique hash to set a cookie in the response headers. (Ask yourself: How do I set cookies using Express?).
 
-// CHECK! If an incoming request has a cookie, the middleware should verify that the cookie is valid (i.e., it is a session that is stored in your database).
-// CHECK !If an incoming cookie is not valid, what do you think you should do with that session and cookie?
+// If an incoming request has a cookie, the middleware should verify that the cookie is valid (i.e., it is a session that is stored in your database).
+// If an incoming cookie is not valid, what do you think you should do with that session and cookie?
 
 
 
